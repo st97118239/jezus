76201bf5ce2fca9c32326482b63b6a91f99291aa
joas <97118239@st.deltion.nl>
2025-05-06 13:17:19 +0200

    NavMeshTest scene gemaakt

---
+++ Assets/Scripts/NavTest/NavigationTest.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+    public List<Transform> waypoints;
+    public NavMeshAgent navMeshAgent;
+    public int currentWPIndex = 0;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        if (waypoints.Count == 0)
+        {
+            return;
+        }
+
+        float distanceToWP = Vector3.Distance(waypoints[currentWPIndex].position, transform.position);
+
+        if (distanceToWP <= 1)
+        {
+            if (currentWPIndex >= waypoints.Count - 1)
+            {
+                //currentWPIndex = 0;
+            }
+            currentWPIndex = (currentWPIndex + 1) % waypoints.Count;
+        }
+
+        navMeshAgent.SetDestination(waypoints[currentWPIndex].position);
+    }
+}
