4abe49c3217a400a55cf37e4e1315e7b07b82021
joas <97118239@st.deltion.nl>
2025-05-13 12:59:58 +0200

    Camera movement toegevoegd en camera een angle gegeven

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public bool cursorLock;
+    public Vector3 moveDir;
+    public float speed;
 
     private void Start()
     {
@@ -10,5 +12,12 @@ public class Movement : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Confined;
         }
+    }
+
+    void Update()
+    {
+        moveDir.z = Input.GetAxis("Horizontal");
+        moveDir.x = -Input.GetAxis("Vertical");
+        transform.Translate(moveDir * Time.deltaTime * speed);
     }
 }
