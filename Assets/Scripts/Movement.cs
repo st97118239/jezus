3e98993ef27c99e06e228387d7d78d1c19bdb8bf
joas <97118239@st.deltion.nl>
2025-05-13 13:54:22 +0200

    camera kan nu inzoemen

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,9 @@ public class Movement : MonoBehaviour
     public bool cursorLock;
     public Vector3 moveDir;
     public float speed;
+    public float zoomSpeed;
+    public Vector3 velocity;
+    public float zoomTime;
 
     private void Start()
     {
@@ -19,5 +22,20 @@ public class Movement : MonoBehaviour
         moveDir.z = Input.GetAxis("Horizontal");
         moveDir.x = -Input.GetAxis("Vertical");
         transform.Translate(moveDir * Time.deltaTime * speed);
+
+        moveDir.z = 0;
+        moveDir.x = 0;
+        float dir = Input.GetAxis("Mouse ScrollWheel");
+        if (dir < 0f)
+            moveDir.y = dir;
+        else if (dir > 0f)
+            moveDir.y = dir;
+
+        //transform.Translate(moveDir * Time.deltaTime * zoomSpeed);
+        //transform.position = Vector3.MoveTowards(transform.position, (moveDir * zoomSpeed), Time.deltaTime);
+        Vector3 desiredPosition = transform.position - (Vector3.up * moveDir.y * zoomSpeed);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, zoomTime);
+        Debug.Log(moveDir * Time.deltaTime * zoomSpeed);
+        moveDir.y = 0;
     }
 }
