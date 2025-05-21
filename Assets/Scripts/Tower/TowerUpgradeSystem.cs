c96869e9a3deebdb00e60a30326f1699e59e480a
joas <97118239@st.deltion.nl>
2025-05-21 11:41:26 +0200

    bezig gegaan met upgrades laten werken

--- Assets/Scripts/Tower/TowerUpgradeSystem.cs
+++ Assets/Scripts/Tower/TowerUpgradeSystem.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerUpgradeSystem : MonoBehaviour
 {
     private GameObject upgradePanel;
     private Tower selectedTower;
+    private TowerUpgrades selectedTowerUpgrades;
+    private Button upgradeButton1;
+    private Button upgradeButton2;
+    private Button upgradeButton3;
+    private List<Button> upgradeButtons;
+
+    private void Start()
+    {
+
+    }
 
     public void FindUpgradePanel(GameObject panel)
     {
@@ -14,6 +27,26 @@ public class TowerUpgradeSystem : MonoBehaviour
     {
         upgradePanel.SetActive(true);
         selectedTower = newTower;
+        selectedTowerUpgrades = selectedTower.GetComponent<TowerUpgrades>();
+
+        
+
+        for (int i = 0; i < 3; i++)
+        {
+            FillUpgrades(i);
+        }
+    }
+
+    private void FillUpgrades(int buttonCount)
+    {
+        buttonCount++;
+        Button selectedButton = upgradeButtons[buttonCount - 1];
+
+        TMP_Text UpgradeButtonText = selectedButton.transform.Find("UpgradeButtonText" + buttonCount).GetComponent<TMP_Text>();
+        TMP_Text UpgradeTextName = selectedButton.transform.Find("UpgradeTextName" + buttonCount).GetComponent<TMP_Text>();
+        TMP_Text UpgradeTextAmount = selectedButton.transform.Find("UpgradeTextAmount" + buttonCount).GetComponent<TMP_Text>();
+        int UpgradeCount = selectedTowerUpgrades.upgrade1Count;
+        int UpgradeMax = selectedTowerUpgrades.upgrade1Max;
     }
 
     public void TowerDeselected()
---
+++ Assets/Scripts/Tower/TowerUpgrades.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class TowerUpgrades : MonoBehaviour
+{
+    public Upgrades upgrade1;
+    public int upgrade1Count;
+    public int upgrade1Max;
+    public float upgrade1Factor;
+    public int upgrade1BaseCost;
+    public float upgrade1CostFactor;
+    public Upgrades upgrade2;
+    public int upgrade2Count;
+    public int upgrade2Max;
+    public float upgrade2Factor;
+    public int upgrade2BaseCost;
+    public float upgrade2CostFactor;
+    public Upgrades upgrade3;
+    public int upgrade3Count;
+    public int upgrade3Max;
+    public float upgrade3Factor;
+    public int upgrade3BaseCost;
+    public float upgrade3CostFactor;
+}
